5d0883474010742456b71419e543d66b8d6815c9
Mac Taylor <Mac@Macs-MacBook-Air.local>
2017-05-10 14:21:29 -0400

    commit 2:21 p.m.

--- js/views/shop.js
+++ js/views/shop.js
@@ -1,2 +1,16 @@
 let View = require('ampersand-view');
 
+module.exports = View.extend({
+    template: document.querySelector('#shop-template').innerHTML,
+    bindings: {
+        'model.tires': '.tires',
+        'model.paint': '.paint',
+        'model.exhaust': '.exhaust',
+        'model.horsepower': '.horsepower',
+        'model.mpg': '.mpg',
+    }, // put this object in 
+    render: function () {
+
+    },
+
+})
