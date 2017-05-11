cfe400d9b426ec2fe52c110ddb3acc03c940fb8b
Mac Taylor <Mac@Macs-MacBook-Air.local>
2017-05-11 16:40:04 -0400

    4:40 p.m.

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ window.addEventListener('load', function () {
     shop.exhaust = false; //'false' means exhuast is broken
     shop.horsepower = 120;
     shop.mpg = 33;
+    shop.totalcost = 0;
 
     let view = new ShopView({
         el: document.querySelector('main'),
