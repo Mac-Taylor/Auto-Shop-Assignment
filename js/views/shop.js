6a0f01e8b832a8936dc16be9c1ebce4fd00b9dc7
Mac Taylor <Mac@Macs-MacBook-Air.local>
2017-05-12 10:08:34 -0400

    think paint buttons should work

--- js/views/shop.js
+++ js/views/shop.js
@@ -39,19 +39,20 @@ module.exports = View.extend({
     // write a changecolor function for each button/color
     changeColorRed: function () {
         // if (not if statement, just English) you hit (color) button change it to that color
-        this.model.color === 'Red';
+        this.model.paint = 'Red';
+
     },
     changeColorGreen: function () {
         // if (not if statement, just English) you hit (color) button change it to that color
-        this.model.color === 'Green';
+        this.model.paint = 'Green';
     },
     changeColorOrange: function () {
         // if (not if statement, just English) you hit (color) button change it to that color
-        this.model.color === 'Orange';
+        this.model.paint = 'Orange';
     },
     changeColorBlack: function () {
         // if (not if statement, just English) you hit (color) button change it to that color
-        this.model.color === 'Black';
+        this.model.paint = 'Black';
     },
 
     render: function () {
